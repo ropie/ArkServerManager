ee08afd4b70ef0ef6d3a4a4f94c775c9f6ca563a
ropie <ropiemasta@gmail.com>
2025-06-01 16:05:25 -0700

    For Jet

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -11,6 +11,8 @@ function Dashboard() {
           For now, I only have the player list working. Links for the other stuff do not work
           yet. But they will soon. :-)
         </h1>
+
+        <img src="https://imageio.forbes.com/specials-images/imageserve/682dfe3765124eff8366ef25/Skibidi-Toilet-is-set-to-become-a-Michael-Bay-film/0x0.jpg?format=jpg&width=960"/>
       </div>
     </>
   );
