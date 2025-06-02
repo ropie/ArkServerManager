91e0d8fcccefe3fd367b324d0513f8d8e76f09fa
ropie <ropiemasta@gmail.com>
2025-06-01 23:55:40 -0700

    dashboard text update

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -8,11 +8,13 @@ function Dashboard() {
       <div className="w-full px-5">
         <h1 className="text-xl text-zinc-800 font-medium">
           This is the Dashboard page. Server stats and other stuff will go here.
-          For now, only links that work are Dashboard and Characters. Links for the other stuff do not work
-          yet. But they will soon. :-)
+          For now, only links that work are Dashboard and Characters. Links for
+          the other stuff do not work yet. But they will soon. :-) Working on
+          pagination for the character list which is why only 25 characters are
+          showing.
         </h1>
 
-        <img src="https://imageio.forbes.com/specials-images/imageserve/682dfe3765124eff8366ef25/Skibidi-Toilet-is-set-to-become-a-Michael-Bay-film/0x0.jpg?format=jpg&width=960"/>
+        <img src="https://imageio.forbes.com/specials-images/imageserve/682dfe3765124eff8366ef25/Skibidi-Toilet-is-set-to-become-a-Michael-Bay-film/0x0.jpg?format=jpg&width=960" />
       </div>
     </>
   );
